7a637eede2388182b7ca5f68f3fbf44d14d79cd2
akrck02 <aketza.vazquez@gmail.com>
2024-07-02 16:40:18 +0200

    adding checking callback

--- models/endpoint.go
+++ models/endpoint.go
@@ -9,16 +9,18 @@ type Endpoint struct {
 	Path     string           `json:"path"`
 	Method   int              `json:"method"`
 	Listener EndpointListener `json:"listener"`
+	Checks   EndpointListener `json:"parameterCheck"`
 	Secured  bool             `json:"secured"`
 }
 
 type EndpointListener func(*gin.Context) (*models.Response, *models.Error)
 
-func EndpointFrom(path string, method int, listener EndpointListener, secured bool) Endpoint {
+func EndpointFrom(path string, method int, listener EndpointListener, checks EndpointListener, secured bool) Endpoint {
 	return Endpoint{
 		Path:     path,
 		Method:   method,
 		Listener: listener,
+		Checks:   checks,
 		Secured:  secured,
 	}
 }
